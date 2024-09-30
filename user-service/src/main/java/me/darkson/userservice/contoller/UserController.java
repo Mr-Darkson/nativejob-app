52241ba01ce48531db3723ddcf4cda95e7033ba4
Mr-Darkson <skybotsi@mail.ru>
2024-09-30 17:33:07 +0300

    create project

---
+++ user-service/src/main/java/me/darkson/userservice/contoller/UserController.java
@@ -0,0 +1,12 @@
+package me.darkson.userservice.contoller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
