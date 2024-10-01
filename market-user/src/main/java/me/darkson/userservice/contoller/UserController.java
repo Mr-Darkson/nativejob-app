ad63a5bdaa27344556cd3e822f3c19a08d23b532
Mr-Darkson <skybotsi@mail.ru>
2024-10-01 16:15:00 +0300

    2 day update, add Cloud Config and fix gateway

--- market-user/src/main/java/me/darkson/userservice/contoller/UserController.java
+++ market-user/src/main/java/me/darkson/userservice/contoller/UserController.java
@@ -1,12 +1,17 @@
 package me.darkson.userservice.contoller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class UserController {
+    @Value("${tmpVar}")
+    private String answer;
+
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return answer;
     }
 }
